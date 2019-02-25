6869d2f7b331c8b855209adcefb558dbc1229598
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-25 09:40:09 -0600

    added work for Views

--- src/main/java/com/codeup/springblog/HomeController.java
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -1,12 +1,19 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
 public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
+
+    @GetMapping("/home")
     public String hello() {
         return "This is the Landing Page!";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public int rollDice(@PathVariable String n){
+        return (int) (Math.random() * 6) + 1;
     }
 }
--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,15 +1,16 @@
 package com.codeup.springblog;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class PostController {
 
     @RequestMapping(path = "/post", method = RequestMethod.GET)
     @ResponseBody
-    public String get(@PathVariable String posts) {
+    public String get() {
         return "posts index page";
     }
 
