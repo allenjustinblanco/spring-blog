e9ee0d820d89421beefaddd206872362a864c7e3
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-25 16:03:42 -0600

    refactor MathController methods to return strings

--- src/main/java/com/codeup/springblog/MathController.java
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -13,28 +13,28 @@ public class MathController {
     public String addOne(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
     }
+
     @RequestMapping(path = "/add/{a}/and/{b}", method = RequestMethod.GET)
     @ResponseBody
-    public int add(@PathVariable int a, @PathVariable int b) {
-        return a + b;
+    public String add(@PathVariable int a, @PathVariable int b) {
+        return a +" plus "+ b + " is " + (a + b);
     }
 
     @RequestMapping(path = "/subtract/{a}/from/{b}", method = RequestMethod.GET)
     @ResponseBody
-    public int subtract(@PathVariable int a, @PathVariable int b) {
-        return b - a;
+    public String subtract(@PathVariable int a, @PathVariable int b) {
+        return b + " minus " + a + " is " + (b - a);
     }
 
     @RequestMapping(path = "/multiply/{a}/and/{b}", method = RequestMethod.GET)
     @ResponseBody
-    public int multiply(@PathVariable int a, @PathVariable int b) {
-        return a + b;
+    public String multiply(@PathVariable int a, @PathVariable int b) {
+        return a + " times " + b + " equals " + (a * b);
     }
 
     @RequestMapping(path = "/divide/{a}/by/{b}", method = RequestMethod.GET)
     @ResponseBody
-    public int divide(@PathVariable int a, @PathVariable int b) {
-        return a + b;
+    public String divide(@PathVariable int a, @PathVariable int b) {
+        return a + " divided by " + b + " equals " + (a / b);
     }
-
 }
