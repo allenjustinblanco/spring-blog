139894f2fae0134b869114d9ff64500c6db73a94
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-03-01 18:37:13 -0600

    Add section for About Me and Rework navbar links

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-});
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.materialboxed').materialbox();
+    $(window).scroll(function(){
+        if($(window).scrollTop()>70){
+            $('nav').removeClass('black').addClass('transparent')
+        } else{
+            $('nav').removeClass('transparent').addClass('black')
+        }
+        if($(window).scrollTop()>500){
+            $('nav').removeClass('transparent').addClass('black')
+        }
+    })
 });
