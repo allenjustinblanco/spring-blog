a22c54390ccf8a78d890be1dceacbc2d8af3312c
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-03-09 21:54:34 -0600

    Commented out UserRepo before implementation

--- src/main/java/com/codeup/springblog/UserRepository.java
+++ src/main/java/com/codeup/springblog/UserRepository.java
@@ -1,6 +1,6 @@
-package com.codeup.springblog;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Users, Long> {
-}
+//package com.codeup.springblog;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface UserRepository extends CrudRepository<Users, Long> {
+//}
